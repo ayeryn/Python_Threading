32155c2c18bc36b84f93ff582cc83063b81e3d69
Sarah Gorring <sarahgorring@gmail.com>
2017-04-23 23:28:48 -0500

    prelim comments for locks

--- locks.py
+++ locks.py
@@ -5,26 +5,30 @@ import random
 
 #We're using loggin to debug. Logging is thread-safe, so messages from different threads are kept distinct in the output.
 logging.basicConfig(level=logging.DEBUG,
-                    format='(%(threadName)-9s) %(message)s',)
+	format='(%(threadName)-9s) %(message)s',)
 
 def try_lock_acquire(lock):
-    logging.debug('Starting')
-    num_tries = 0
-    while num_tries < 2:
-        time.sleep(0.5)
-        logging.debug('Trying to acquire')
-        have_it = lock.acquire(0)
-        try:
-            num_tries += 1
-            if have_it:
-                logging.debug('Iteration %d: Acquired',  num_tries)
-            else:
-                logging.debug('Iteration %d: Not acquired', num_tries)
-        finally:
-            if have_it:
-                lock.release()
-                logging.debug('Iteration %d: Lock released', num_tries)
-    logging.debug('Done after %d iterations', num_tries)
+	""" both threads will call this function to try to acquire the lock"""
+	logging.debug('Starting')
+	for i in range(2):
+		time.sleep(0.5)
+		logging.debug('Trying to acquire')
+
+		""" a tread calling .acquire() will hold the lock if the lock currently has no other treads holding it"""
+		""" The floating-point argument will cause the thread to block for at most the number of seconds specified by timeout
+		and as long as the lock cannot be acquired. """
+		""" Returns true if the lock has been acquired, false if the timeout has elapsed."""
+		have_it = lock.acquire(0)
+		try:
+			if have_it:
+				logging.debug('Iteration %d: Acquired',  i+1)
+			else:
+				logging.debug('Iteration %d: Not acquired', i+1)
+		finally:
+			if have_it:
+				lock.release()
+				logging.debug('Iteration %d: Lock released', i+1)
+				logging.debug('Thread %s done after %d iterations', threading.currentThread().getName(), i+1)
 
 
 lock = threading.Lock()
@@ -34,6 +38,8 @@ thread1.start()
 thread2 = threading. Thread(target=try_lock_acquire, args=(lock,), name='thread2')
 thread2.start()
 
+
+""" output will differ every time the program is run due to the nature of threads."""
 # (thread1  ) Starting
 # (thread2  ) Starting
 # (thread1  ) Trying to acquire
@@ -41,10 +47,10 @@ thread2.start()
 # (thread1  ) Iteration 1: Acquired
 # (thread2  ) Iteration 1: Not acquired
 # (thread1  ) Iteration 1: Lock released
+# (thread1  ) Done after 1 iterations
 # (thread2  ) Trying to acquire
 # (thread1  ) Trying to acquire
 # (thread2  ) Iteration 2: Acquired
 # (thread1  ) Iteration 2: Not acquired
 # (thread2  ) Iteration 2: Lock released
-# (thread1  ) Done after 2 iterations
 # (thread2  ) Done after 2 iterations
